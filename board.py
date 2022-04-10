4ac4ad214197eb1595ab143f8e576a1c2fb29211
nassorh <nassorh.dev@gmail.com>
2022-04-10 21:18:47 +0100

    cleaned up code

--- board.py
+++ board.py
@@ -1,86 +1,94 @@
+from tkinter import HORIZONTAL
+from xmlrpc.client import boolean
 import numpy as np
+from pandas import array
+import re 
 
 class Board():
-    def __init__(self):
-        self.board = [
-                [5,3,0,0,7,0,0,0,0],
-                [6,0,0,1,9,5,0,0,0],
-                [0,9,8,0,0,0,0,6,0],
-                [8,0,0,0,6,0,0,0,3],
-                [4,0,0,8,0,3,0,0,1],
-                [7,0,0,0,2,0,0,0,6],
-                [0,6,0,0,0,0,2,8,0],
-                [0,0,0,4,1,9,0,0,5],
-                [0,0,0,0,8,0,0,7,9]
-            ]
-
-        self.solution = [
-                    [5,3,4,6,7,8,9,1,2],
-                    [6,7,2,1,9,5,3,4,8],
-                    [1,9,8,3,4,2,5,6,7],
-                    [8,5,9,7,6,1,4,2,3],
-                    [4,2,6,8,5,3,7,9,1],
-                    [7,1,3,9,2,4,8,5,6],
-                    [9,6,1,5,3,7,2,8,4],
-                    [2,8,7,4,1,9,6,3,5],
-                    [3,4,5,2,8,6,1,7,9]
-            ]
+    def __init__(self,board_dir):
+        self.board = self.convert_board_to_array(board_dir)
 
         #Len of row for each block
         self.rowLen = 3
         #Len of col for each block
         self.colLen = 3
 
-    def displayBoard(self):
+    def displayBoard(self) -> None:
         print(np.matrix(self.board))
 
-    def findEmptyCell(self):
+    def find_next_empty_cell(self):
+        #ROW AND COL IN DATA STRCUTURE
+        #RE CODE THIS
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
                 if self.board[row][column] == 0:
                     return row,column
         return False
 
-    def fetchAllNeigbourNums(self,row,col):
-        numbersFound = dict()
-        #Horizontal test
+    def fetch_all_neigbour_nums(self,row,col) -> dict:
+        numbersFound= {}
+        
+        horizontal_nums = self.get_horizontal_nums(row)
+        vertical_nums = self.get_vertical_nums(col)
+        block_nums = self.get_nums_in_block(row,col)    
+        
+        numbersFound.update(horizontal_nums)
+        numbersFound.update(vertical_nums)
+        numbersFound.update(block_nums)
+
+        return numbersFound
+    
+    def get_horizontal_nums(self,row) -> dict:
+        numbersFound_boolean = dict()
         for x in range(len(self.board[row])):
             cell = self.board[row][x]
-            if numbersFound.get(cell) == None and cell!=0:
-                numbersFound[cell] = True
-            
-        #Vertical test
+            if numbersFound_boolean.get(cell) == None and cell!=0:
+                numbersFound_boolean[cell] = True
+        return numbersFound_boolean
+    
+    def get_vertical_nums(self,col) -> dict:
+        numbersFound_boolean = dict()
         for x in range(len(self.board)):
             cell = self.board[x][col]
-            if numbersFound.get(cell) == None and cell!=0:
-                numbersFound[cell] = True
+            if numbersFound_boolean.get(cell) == None and cell!=0:
+                numbersFound_boolean[cell] = True
+        return numbersFound_boolean
+    
+    def get_nums_in_block(self,row,col) -> dict:
+        numbersFound_boolean = dict()
 
-        #Block loop        
         boxRowStart=(row//3)*3
         colRowStart=(col//3)*3
         for x in range(boxRowStart,boxRowStart+3):
             for y in range(colRowStart,colRowStart+3):
                 cell = self.board[x][y]
-                if numbersFound.get(cell) == None and cell!=0:
-                    numbersFound[cell] = True
+                if numbersFound_boolean.get(cell) == None and cell!=0:
+                    numbersFound_boolean[cell] = True
                     
-        return numbersFound
-    
-    def ifValid(self,row,col,num):
-        numbersFound = self.fetchAllNeigbourNums(row,col)
+        return numbersFound_boolean
+
+    def ifValid(self,row,col,num) -> boolean:
+        numbersFound = self.fetch_all_neigbour_nums(row,col)
         if numbersFound.get(num)==None:
             return True
         else:
             return False
 
-    def updateVal(self,row,col,val):
+    def updateVal(self,row,col,val) -> boolean:
         self.board[row][col] = val
         return True
 
-    def testAnswer(self):
-        boardLen = len(self.board)
-        count = 0
-        for x in range(boardLen):
-            if self.board[x] == self.solution[x]:
-                count+=1
-        return count==boardLen
+    @classmethod
+    def convert_board_to_array(cls,filedir) -> array:
+        board_array = []
+        with open(filedir) as file:
+            lines = file.readlines()
+            for line in lines:
+                nums = []
+                line = line.strip().split(",")
+                for num in line:
+                    nums.append(int(num))
+                board_array.append(nums)
+        return board_array
+
+board = Board.convert_board_to_array("board.txt")
