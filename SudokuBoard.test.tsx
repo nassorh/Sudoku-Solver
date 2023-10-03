7bcc5fd9489fb51e61152159dee4c612de25b3e9
nassorh <nassorh.dev@gmail.com@ --global>
2023-10-03 18:09:09 +0100

    test: Update Tests for New Logic and Addition of Values/Notes to Fixed Cells
    - Refactored existing tests to align with the updated logic.
    - Introduced tests for adding values and notes to fixed cells.

--- SudokuBoard.test.tsx
+++ SudokuBoard.test.tsx
@@ -37,6 +37,12 @@ describe('SudokuBoard Cell Values', () => {
     expect(sudokuBoard.getCellNotes(0, 3)).toEqual(new Set());
     expect(sudokuBoard.getCellValue(0, 3)).toBe(3);
   });
+
+  test('when adding a value to a fixed value, it should not update', () => {
+    const sudokuBoard = new SudokuBoard(sudokuArray, size, boxSize);
+    sudokuBoard.setCellValue(0, 0, 6)
+    expect(sudokuBoard.getCellValue(0,0)).toEqual(5);
+  });
 });
 
 describe('SudokuBoard Cell Notes', () => {
@@ -53,12 +59,12 @@ describe('SudokuBoard Cell Notes', () => {
   test('when adding a note, it should remove the value', () => {
     const sudokuBoard = new SudokuBoard(sudokuArray, size, boxSize);
     
-    sudokuBoard.setCellValue(0, 4, 7)
-    expect(sudokuBoard.getCellValue(0, 4)).toBe(7);
+    sudokuBoard.setCellValue(0, 3, 7)
+    expect(sudokuBoard.getCellValue(0, 3)).toBe(7);
 
-    sudokuBoard.addCellNotes(0, 4, 6)
-    expect(sudokuBoard.getCellValue(0, 4)).toBe(null);
-    expect(sudokuBoard.getCellNotes(0, 4)).toEqual(new Set([6]));
+    sudokuBoard.addCellNotes(0, 3, 6)
+    expect(sudokuBoard.getCellValue(0, 3)).toBe(null);
+    expect(sudokuBoard.getCellNotes(0, 3)).toEqual(new Set([6]));
   });
 
   test('removing a note where a value is and checking its effect', () => {
@@ -67,25 +73,25 @@ describe('SudokuBoard Cell Notes', () => {
      sudokuBoard.removeCellNote(0, 0, 5)
      expect(sudokuBoard.getCellValue(0, 0)).toBe(5);
   });
+
+  test('when adding a note to a fixed value, it should not update', () => {
+    const sudokuBoard = new SudokuBoard(sudokuArray, size, boxSize);
+    sudokuBoard.addCellNotes(0, 0, 6)
+    expect(sudokuBoard.getCellNotes(0,0)).toEqual(new Set());
+  });
 });
 
 describe('SudokuBoard resetting', () => {
   test('resetting the board', () => {
     const sudokuBoard = new SudokuBoard(sudokuArray, size, boxSize);
-    
-     sudokuBoard.setCellValue(0, 0, null)
-     expect(sudokuBoard.getCellValue(0, 0)).toBe(null);
-
      sudokuBoard.setCellValue(0, 3, 7)
      expect(sudokuBoard.getCellValue(0, 3)).toBe(7);
 
-     sudokuBoard.addCellNotes(0, 0, 1)
-     expect(sudokuBoard.getCellNotes(0, 0)).toEqual(new Set([1]));
+     sudokuBoard.addCellNotes(0, 2, 1)
+     expect(sudokuBoard.getCellNotes(0, 2)).toEqual(new Set([1]));
 
      sudokuBoard.reset(sudokuArray)
-     expect(sudokuBoard.getCellValue(0, 0)).toBe(5);
      expect(sudokuBoard.getCellValue(0, 3)).toBe(null);
-     expect(sudokuBoard.getCellNotes(0, 0)).toEqual(new Set());
-     
+     expect(sudokuBoard.getCellNotes(0, 2)).toEqual(new Set());
   });
 });
