6fb0583fae0f5ae08a7791e2fdef982439f8fa76
nassorh <nassorh.dev@gmail.com@ --global>
2023-10-14 18:59:14 +0100

    Refactor : Added Icons to game control buttons
    - Added Icons to control buttons
    - Defined the call back functions in the interface

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -1,31 +1,54 @@
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import NotesIcon from '@mui/icons-material/Notes';
+import Typography from '@mui/material/Typography';
+
 interface GameControlsProps{
-    onClearNumber : Function
-    onClickNotes : Function
+    onClearNumber : () => void;
+    onClickNotes : () => void;
     clickStatus : boolean
-    onClickUndo : Function
-    onClickRedo : Function
-    onClickNewGame : Function
+    onClickUndo : () => void;
+    onClickRedo : () => void;
 } 
 
 const GameControls = (props : GameControlsProps) => {
     return (
-      <div>
-          <button key="new" onClick={() => props.onClickNewGame()}>
-            New Game
-          </button>
-          <button key="undo" onClick={() => props.onClickUndo()}>
-            undo
-          </button>
-          <button key="clear" onClick={() => props.onClearNumber()}>
-            clear
-          </button>
-          <button key="notes" onClick={() => props.onClickNotes()}>
-            notes {props.clickStatus.toString()}
-          </button>
-          <button key="redo" onClick={() => props.onClickRedo()}>
-            redo
-          </button>
-      </div>
+      <div className='game-controls'>
+        
+        <div>
+          <IconButton className="game-control-icon" key="undo" onClick={() => props.onClickUndo()} aria-label="Undo">
+            <UndoIcon />
+          </IconButton>
+          <Typography variant="button">Undo</Typography>
+        </div>
+
+        <div >
+          <IconButton className="game-control-icon" key="clear" onClick={() => props.onClearNumber()} aria-label="Clear">
+            <ClearIcon />
+          </IconButton>
+          <Typography variant="button">Clear</Typography>
+        </div>
+
+        <div>
+            {props.clickStatus?
+              <p className="notes-status">on</p>
+              :<p className="notes-status">off</p>
+            }
+          <IconButton className="game-control-icon" key="notes" onClick={() => props.onClickNotes()} aria-label="Notes">
+            <NotesIcon />
+          </IconButton>
+          <Typography variant="button">Notes</Typography>
+        </div>
+
+        <div>
+          <IconButton className="game-control-icon"key="redo" onClick={() => props.onClickRedo()} aria-label="Redo">
+            <RedoIcon />
+          </IconButton>
+          <Typography variant="button">Redo</Typography>
+        </div>
+    </div>
     );
   };
   
